cedfd67015fc6ead211090378201df8dde59fcd1
Sergio Ramirez <gc.desarrollo@outlook.com>
2023-11-05 12:41:43 -0600

    chore: directorio raiz de frontend movido y build finalizada

--- src/main/java/com/practicaweb/apirest/ApiRestApplication.java
+++ src/main/java/com/practicaweb/apirest/ApiRestApplication.java
@@ -2,27 +2,34 @@ package com.practicaweb.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ApiRestApplication {
+public class ApiRestApplication
+        extends SpringBootServletInitializer
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestApplication.class, args);
+    }
 
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080", "http://localhost:3000");
-			}
-		};
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiRestApplication.class);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:3000");
+            }
+        };
+    }
 
 }
